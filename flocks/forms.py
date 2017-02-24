364b21638961ba86b0bd044d70df68bdb7c85e7d
forcaeluz <forcaeluz@gmail.com>
2017-02-24 20:41:13 +0100

    Added animal death form, and also limited the number of choices in Exits and Deaths to flocks were there are living animals.

--- flocks/forms.py
+++ flocks/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.core.validators import ValidationError
+
+from flocks.models import AnimalDeath
 from .models import AnimalExits, Flock
 
 
@@ -11,11 +13,16 @@ class FlockForm(forms.Form):
 
 class AnimalExitsForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(AnimalExitsForm, self).__init__(*args, **kwargs)
+        current_flocks = [obj.id for obj in Flock.objects.all() if obj.number_of_living_animals > 0]
+        self.fields['flock'].queryset = Flock.objects.filter(pk__in=current_flocks)
+
     def clean_number_of_animals(self):
         n_of_animals = self.cleaned_data.get('number_of_animals')
         flock = self.fields['flock'].queryset[0]
         if n_of_animals > flock.number_of_living_animals:
-            raise ValidationError(("Number of exiting animals is bigger than available."), code='Too much animals')
+            raise ValidationError("Number of exiting animals is bigger than available.", code='Too much animals')
 
         return n_of_animals
 
@@ -23,3 +30,13 @@ class AnimalExitsForm(forms.ModelForm):
         model = AnimalExits
         fields = ['date', 'number_of_animals', 'total_weight', 'flock']
 
+
+class AnimalDeathForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AnimalDeathForm, self).__init__(*args, **kwargs)
+        current_flocks = [obj.id for obj in Flock.objects.all() if obj.number_of_living_animals > 0]
+        self.fields['flock'].queryset = Flock.objects.filter(pk__in=current_flocks)
+
+    class Meta:
+        model = AnimalDeath
+        fields = ['date', 'weight', 'cause', 'flock']
