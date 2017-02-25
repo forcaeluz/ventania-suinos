a5227bdad1b8d4e1b99844acda89cbca2ecc6dfb
forcaeluz <forcaeluz@gmail.com>
2017-02-25 21:01:06 +0100

    Added farm app. It basically gatters the information of multiple apps.

--- Suinos/urls.py
+++ Suinos/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^flocks/', include('flocks.urls')),
     url(r'^feeding/', include('feeding.urls')),
+    url(r'^$', include('farm.urls')),
     url(r'^admin/', admin.site.urls),
 ]
