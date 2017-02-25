4e95dbe560ca742d67fcccff6db0b89a1b39feae
forcaeluz <forcaeluz@gmail.com>
2017-02-25 21:00:52 +0100

    Added farm app. It basically gatters the information of multiple apps.

---
+++ farm/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from feeding.models import FeedType
+from flocks.models import Flock
+from statistics import mean
+
+
+def index(request):
+    feeding_types = FeedType.objects.all()
+    flocks = [obj for obj in Flock.objects.all() if obj.number_of_living_animals > 0]
+    n_animals = sum([flock.number_of_living_animals for flock in flocks])
+    death_perc = mean([flock.death_percentage for flock in flocks])
+    params = {
+        'flocks': flocks,
+        'feed_types': feeding_types,
+        'n_animals': n_animals,
+        'death_perc': death_perc,
+    }
+    return render(request, 'farm/index.html', params)
