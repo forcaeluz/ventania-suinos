01cb7525c9f420cf39eee5799ed8c08a96accf45
forcaeluz <forcaeluz@gmail.com>
2017-03-04 14:31:24 +0100

    Added the static files to the farm app. Also added a welcome page (for the case when there is no data saved yet.)

--- farm/views.py
+++ farm/views.py
@@ -7,6 +7,9 @@ from statistics import mean
 def index(request):
     feeding_types = FeedType.objects.all()
     flocks = [obj for obj in Flock.objects.all() if obj.number_of_living_animals > 0]
+    if len(flocks) == 0 or feeding_types.count() == 0:
+        return render(request, 'farm/welcome.html')
+
     n_animals = sum([flock.number_of_living_animals for flock in flocks])
     death_perc = mean([flock.death_percentage for flock in flocks])
     params = {
