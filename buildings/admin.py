a7749d0bba179a5a861e66ed9cc1782cebd6887f
forcaeluz <forcaeluz@gmail.com>
2017-05-26 11:41:48 +0200

    Added more to the admin site. This is usefull to fix issues due to malfunctioning of the wizard.

--- buildings/admin.py
+++ buildings/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Room, Building, RoomGroup, Silo
+from .models import Room, Building, RoomGroup, Silo, AnimalRoomEntry, AnimalRoomExit
 # Register your models here.
 
 admin.site.register(Room)
 admin.site.register(RoomGroup)
 admin.site.register(Building)
 admin.site.register(Silo)
+admin.site.register(AnimalRoomEntry)
+admin.site.register(AnimalRoomExit)
+
+
