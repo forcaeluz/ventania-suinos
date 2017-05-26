33a593f086d8969a26f1380f8ec721d48a5660bf
forcaeluz <forcaeluz@gmail.com>
2017-05-26 11:21:03 +0200

    Improve speed when loading EntryRoomForms.

--- buildings/forms.py
+++ buildings/forms.py
@@ -32,6 +32,7 @@ class FlockEntryRoomForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['room'].queryset = Room.objects.filter(id=kwargs['initial']['room'].id)
         for field in iter(self.fields):
             self.fields[field].widget.attrs.update({
                 'class': 'form-control',
@@ -39,6 +40,7 @@ class FlockEntryRoomForm(forms.ModelForm):
             if self.fields[field].error_messages:
                 self.fields[field].widget.attrs.update({'class': 'form-control'})
         self.fields['room'].widget.attrs['readonly'] = True
+
         flocks = [obj.id for obj in Flock.objects.all() if obj.number_of_living_animals > 0]
         self.fields['flock'].queryset = Flock.objects.filter(pk__in=flocks)
 
