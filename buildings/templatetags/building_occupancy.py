c62e2e4d2062c11acef151d0300ff5a55027fd08
forcaeluz <forcaeluz@gmail.com>
2017-08-08 20:41:30 +0200

    Added progress bars indicating feed stock and consumption.

--- buildings/templatetags/building_occupancy.py
+++ buildings/templatetags/building_occupancy.py
@@ -1,10 +1,10 @@
 from django import template
-
+from datetime import date
 
 register = template.Library()
 
 
-@register.inclusion_tag('buildings/progressbar.html')
+@register.inclusion_tag('buildings/tags/progressbar.html')
 def room_occupancy_bar(room):
     return {'name': room.name,
             'occupancy': room.occupancy,
@@ -12,6 +12,19 @@ def room_occupancy_bar(room):
             'overcapacity': room.occupancy - room.capacity}
 
 
-@register.inclusion_tag('buildings/room_group_occupancy.html')
+@register.inclusion_tag('buildings/tags/room_group_occupancy.html')
 def room_group_occupancy(group):
     return {'group': group}
+
+
+@register.inclusion_tag('buildings/tags/feed_progressbar.html')
+def feed_remains(feed_type, building):
+    remaining = building.get_estimated_remaining_feed(at_date=date.today(), feed_type=feed_type)
+    consumption = building.get_average_feed_consumption(at_date=date.today(), feed_type=feed_type)
+    capacity = building.feed_capacity(feed_type)
+    return {'name': feed_type.name,
+            'capacity': capacity,
+            'remaining': remaining,
+            'consumption': consumption,
+            'remaining_after_consumption': remaining - consumption
+            }
