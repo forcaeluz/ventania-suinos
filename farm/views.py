a7035e9524c0d3b02b00393590e8dda8d4268c23
forcaeluz <forcaeluz@gmail.com>
2017-08-11 22:31:21 +0200

    Implemented new Kpi's in farm index.

--- farm/views.py
+++ farm/views.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 
 from feeding.models import FeedType
 from flocks.models import Flock, AnimalSeparation, AnimalDeath, AnimalFlockExit, AnimalFarmExit
+from flocks.kpis import NumberOfAnimalsKpi, DeathPercentageKpi, SeparationsKpi, GrowRateKpi
 from buildings.models import Room
 
 
@@ -25,6 +26,7 @@ from .models import AnimalEntry, AnimalExit
 
 
 class FarmKpi:
+
     def __init__(self, kpi_class, title, value, unit):
         self.kpi_class = kpi_class
         self.title = title
@@ -61,54 +63,22 @@ class FarmIndexView(TemplateView):
     def generate_kpi_data(self):
         kpi_list = []
         kpi_list.extend(self.generate_flock_kpis())
-        kpi_list.extend(self.generate_historic_kpis())
         return kpi_list
 
-    def generate_flock_kpis(self):
+    @staticmethod
+    def generate_flock_kpis():
         """
         Generates the FarmKpi for the flocks currently in the farm.
         :return:
         """
         kpi_list = []
 
-        number_of_animals = sum([obj.number_of_animals for obj in self.current_flocks])
-        number_of_dead_animals = sum([obj.animaldeath_set.count() for obj in self.current_flocks])
-        death_percentage = number_of_dead_animals / number_of_animals * 100
-        number_of_separated_animals = len(self.active_separations)
-
-        separation_percentage = number_of_separated_animals / number_of_animals * 100
-        kpi_list.append(FarmKpi('success', 'Animals on Farm', self.number_of_living_animals, ''))
-
-        if self.farm_capacity is not 0:
-            occupancy = (self.number_of_living_animals * 100 / self.farm_capacity)
-            kpi_list.append(FarmKpi('success', 'Occupancy', '%.2f' % occupancy, '%'))
-
-        kpi_death_perc = FarmKpi('danger', 'Death Percentage', '%.2f' % death_percentage, '%')
-        if death_percentage < 2:
-            kpi_death_perc.kpi_class = 'success'
-        elif death_percentage < 5:
-            kpi_death_perc.kpi_class = 'warning'
-        kpi_list.append(kpi_death_perc)
-        kpi_list.append(FarmKpi('warning', 'Animal Separation', '%.2f' % separation_percentage, '%'))
+        kpi_list.append(NumberOfAnimalsKpi())
+        kpi_list.append(DeathPercentageKpi())
+        kpi_list.append(SeparationsKpi())
+        kpi_list.append(GrowRateKpi())
 
         return kpi_list
-
-    def generate_historic_kpis(self):
-        considering_from = datetime.today() - timedelta(days=365)
-        flocks_exited_past_year = AnimalFlockExit.objects.filter(flock__animalflockexit__farm_exit__date__gt=considering_from)
-        if not flocks_exited_past_year:
-            return []
-
-        number_of_animals = sum([obj.number_of_animals for obj in flocks_exited_past_year])
-        weighted_grow_rate = sum([obj.grow_rate * obj.number_of_animals for obj in flocks_exited_past_year])
-        grow_rate = weighted_grow_rate / number_of_animals
-        kpi_grow_rate = FarmKpi('success', 'Grow Rate', '%.2f' % grow_rate, 'kg/day')
-        if grow_rate < 0.700:
-            kpi_grow_rate.kpi_class = 'danger'
-        elif grow_rate < 0.850:
-            kpi_grow_rate.kpi_class = 'warning'
-
-        return [kpi_grow_rate]
 
     def generate_warnings(self):
         # No warnings for the time being.
