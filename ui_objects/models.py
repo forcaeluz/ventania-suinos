256722e87ef6e520d5ceb9f5b9c5f55ce2861e86
forcaeluz <forcaeluz@gmail.com>
2017-08-11 22:23:14 +0200

    Implemented KPI code, with a small test.

---
+++ ui_objects/models.py
@@ -0,0 +1,13 @@
+
+
+class Kpi:
+    value = None
+    color = None
+    description = None
+    action_name = None
+    action = None
+    icon = None
+
+
+class InfoKpi(Kpi):
+    color = 'primary'
