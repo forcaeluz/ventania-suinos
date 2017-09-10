295a96f9bddbd0667c3f764ca4479c89e98fb890
forcaeluz <forcaeluz@gmail.com>
2017-09-10 18:22:54 +0200

    Document new classes.

--- buildings/templatetags/building_occupancy.py
+++ buildings/templatetags/building_occupancy.py
@@ -6,6 +6,11 @@ register = template.Library()
 
 @register.inclusion_tag('buildings/tags/progressbar.html')
 def room_occupancy_bar(room):
+    """ Template tag for the progress bar that shows room occupancy.
+
+    :param room: The room
+    :return: A dictionary used in the progressbar template.
+    """
     return {'name': room.name,
             'occupancy': room.occupancy,
             'capacity': room.capacity,
@@ -14,11 +19,22 @@ def room_occupancy_bar(room):
 
 @register.inclusion_tag('buildings/tags/room_group_occupancy.html')
 def room_group_occupancy(group):
+    """ Template tag for the table for progress-bars for a room groupd.
+
+    :param group: The room group.
+    :return: A dict for the room-group used in the template.
+    """
     return {'group': group}
 
 
 @register.inclusion_tag('buildings/tags/feed_progressbar.html')
 def feed_remains(feed_type, building):
+    """ Template tag for the progress bar for feed availability.
+
+    :param feed_type: The feed type for which information is desired.
+    :param building: The building.
+    :return: A dict used in the template, with the usage/remaining information.
+    """
     remaining = building.get_estimated_remaining_feed(at_date=date.today(), feed_type=feed_type)
     consumption = building.get_average_feed_consumption(at_date=date.today(), feed_type=feed_type)
     capacity = building.feed_capacity(feed_type)
